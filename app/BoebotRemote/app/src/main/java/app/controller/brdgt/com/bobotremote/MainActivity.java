b65bce6b4cf100e4dcae22870b10b8b7e609c3e6
Bridget Woodye <BrdgtColleenW@gmail.com>
2017-10-13 21:03:51 -0400

    fixed buttons
    transmits text changes

--- app/BoebotRemote/app/src/main/java/app/controller/brdgt/com/bobotremote/MainActivity.java
+++ app/BoebotRemote/app/src/main/java/app/controller/brdgt/com/bobotremote/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,73 +13,84 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button start = (Button) findViewById(R.id.start_button);
+        final TextView transmit = (TextView) findViewById(R.id.input_output);
+
+        Button start = (Button) findViewById(R.id.start_button);
         start.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Begin Instructions");
+            public void onClick(View view) {
+                // tells user that the machine can accept commands now
+                transmit.setText("Intaking");
             }
         });
 
-        final Button end = (Button) findViewById(R.id.end_button);
+        Button end = (Button) findViewById(R.id.end_button);
         end.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Stop Instructions");
+            public void onClick(View view) {
+                transmit.setText("Outputting");
             }
         });
 
-        final Button up = (Button) findViewById(R.id.up);
+        final TextView command_print = (TextView) findViewById(R.id.command_printer);
+
+        Button up = (Button) findViewById(R.id.up);
         up.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Forward");
+            public void onClick(View view) {
+                command_print.setText("Forward");
             }
         });
 
-        final Button left = (Button) findViewById(R.id.left_arrow);
+        Button left = (Button) findViewById(R.id.left_arrow);
         left.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Left");
+            public void onClick(View view) {
+                command_print.setText("Left");
             }
         });
 
-        final Button right = (Button) findViewById(R.id.right_arrow);
+        Button right = (Button) findViewById(R.id.right_arrow);
         right.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Right");
+            public void onClick(View view) {
+                command_print.setText("Right");
             }
         });
 
-        final Button down = (Button) findViewById(R.id.down_arrow);
+        Button down = (Button) findViewById(R.id.down_arrow);
         down.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Back");
+            public void onClick(View view) {
+                command_print.setText("Back");
             }
         });
 
-        final Button delay = (Button) findViewById(R.id.delay);
+        Button delay = (Button) findViewById(R.id.delay);
         delay.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Delay");
+            public void onClick(View view) {
+                command_print.setText("Delay");
             }
         });
 
-        final Button leftTurn = (Button) findViewById(R.id.turn_left);
+        Button leftTurn = (Button) findViewById(R.id.turn_left);
         leftTurn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Turn Left");
+            public void onClick(View view) {
+                command_print.setText("Turn Left");
             }
         });
 
-        final Button rightTurn = (Button) findViewById(R.id.turn_right);
+        Button rightTurn = (Button) findViewById(R.id.turn_right);
         rightTurn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Turn Right");
+            public void onClick(View view) {
+                command_print.setText("Turn Right");
             }
         });
 
-        final Button connect = (Button) findViewById(R.id.connector);
-        connect.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Connected");
+        final TextView connect = (TextView) findViewById(R.id.power);
+
+        Button connection = (Button) findViewById(R.id.connector);
+        connection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                // establishes connection with bluetooth
+                // makes sure the boebot is on
+                connect.setText("On");
             }
         });
     }
